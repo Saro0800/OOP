db5424d2a840c1dfaa62cbac07c43e389629be93
Saro0800 <56132667+Saro0800@users.noreply.github.com>
2020-05-20 09:03:45 +0200

    Update User.java

--- OOP_LAB_Diet/src/diet/User.java
+++ OOP_LAB_Diet/src/diet/User.java
@@ -5,13 +5,25 @@ package diet;
  *  
  */
 public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;	
+	
+	public User (String firstName, String lastName, String email, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phoneNumber;
+	}
 		
 	/**
 	 * get user's last name
 	 * @return last name
 	 */
 	public String getLastName() {
-		return null;
+		return lastName;
 	}
 	
 	/**
@@ -19,7 +31,7 @@ public class User {
 	 * @return first name
 	 */
 	public String getFirstName() {
-		return null;
+		return firstName;
 	}
 	
 	/**
@@ -27,7 +39,7 @@ public class User {
 	 * @return email
 	 */
 	public String getEmail() {
-		return null;
+		return email;
 	}
 	
 	/**
@@ -35,7 +47,7 @@ public class User {
 	 * @return  phone number
 	 */
 	public String getPhone() {
-		return null;
+		return phone;
 	}
 	
 	/**
@@ -43,6 +55,7 @@ public class User {
 	 * @param email new email
 	 */
 	public void SetEmail(String email) {
+		this.email=email;
 	}
 	
 	/**
@@ -50,6 +63,11 @@ public class User {
 	 * @param phone new phone number
 	 */
 	public void setPhone(String phone) {
+		this.phone=phone;
+	}
+	
+	public String toString() {
+		return (this.firstName+" "+this.lastName);
 	}
 	
 }
